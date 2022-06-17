5af49942a4d22da332a4c858452984bcd6d9100e
ROPAN1991 <91415292+ROPAN1991@users.noreply.github.com>
2022-06-17 15:34:49 +0300

    newcommit

--- js/objects.js
+++ js/objects.js
@@ -289,3 +289,189 @@
 // console.log(book.originalLanguage); // 'en'
 // console.log(book.translations); // ['ua', 'ru']
 
+// ------------------колбекі
+
+//  
+// doMath (7,3, function (x,y) {
+//     return x - y;
+// }
+// )
+// doMath (5,2, function (x,y) {
+//     return x * y;
+// }
+// )
+
+// ----------button--------кількість кліків по кнопці------------реєстрація події
+
+
+// const buttonref = document.querySelector('.js-button');
+
+// const handleButtonClick = function() {
+//     console.log('THIS IS BUTTON CLICK' + Date.now())
+// }
+
+// buttonref.addEventListener('click', handleButtonClick)
+
+
+
+// --------------отримання-геопозиції-користувача------------------
+
+// const onGetPositionSucces = function(position) {
+// console.log('Це виклик oneGetPositionSucces');
+//     console.log(position);
+// };
+
+// const onGetPositionError = function(error) {
+// console.log('Це виклик oneGetPositionError')
+//     console.log(error);
+// };
+
+// window.navigator.geolocation.getCurrentPosition(onGetPositionSucces, onGetPositionError);
+
+// ------------------------відкладений виклик: інтервали--------------
+
+// const callback = function () {
+//     console.log('Через3 секунди в середині колбека відбудеться подія')
+// }
+// console.log('в коді перед таймаутом')
+// setTimeout(callback, 3000)
+// console.log('в коді після таймаута')
+
+// -----------------------функція знаходить більші і меньші числа------------------функція фільтрації по масиву----------------
+
+// const filter = function (array, test) {
+//     const filteredArray = [];
+
+//     for (const el of array) {
+//         console.log(el)
+//         const passed = test(el);
+
+//         if (passed) {
+//             filteredArray.push(el)
+//         }
+//     }
+//     return filteredArray
+// }
+
+// const callback1 = function(value) {
+//     return value >= 3
+// };
+// const callback2 = function(value) {
+//     return value <= 4;
+// };
+
+//  const r1 = filter([1,2,3,4,5], callback1) 
+//  console.log(r1)
+
+//  const r2 = filter([1,2,3,4,5,6,7], callback2)
+//  console.log(r2)
+
+//  const fruits = [
+//     { name: 'apple', quantity: 200, isFresh: true },
+//     { name: 'grapes', quantity: 150, isFresh: false },
+//     { name: 'bananas', quantity: 100, isFresh: true },
+//  ];
+//  const getFruitsWithQuantity = function(fruit) {
+//     return fruit.quantity >= 120;
+
+//  }
+     
+//  const r3 = filter(fruits, getFruitsWithQuantity);
+
+//  console.log(r3)
+
+// --------------------функція замикання------------------
+// const makeSheff = function (name) {
+//     const makeDish = function (dish) {
+//     console.log(`${name} готує ${dish}`);
+//     }
+//     return makeDish
+    
+//     }
+
+//     const mango = makeSheff('Mango');
+
+//     mango('пиріжки')
+//     mango('салат')
+
+// -------------------------------округлювач-----------------------------------------
+
+// const rounder = function (places) {
+//     return function (number) {
+//         return Number(number.toFixed(places));
+//     };
+    
+// }
+
+// const rounder1 = rounder(2);
+// const rounder2 = rounder(3);
+
+// console.log(rounder1(5.83456));
+// console.log(rounder2(7.3894756));
+// console.log(rounder1(2.7654867));
+// console.log(rounder2(4.97567));
+
+// ---------------------------- зарплата----------------------------
+
+// const salaryManagerFactor = function (employeeName, baseSalary) {
+//     let salary = baseSalary;
+
+// return {
+//     raise(amount) {
+//         if (amount > 1000) {
+//             return "Ти офігів?";
+//         }
+//         salary += amount
+//     },
+//     lower(amount) {
+//         salary -= amount
+//     },
+//     current() {
+//         return `Поточна заробітна плата ${employeeName} - ${salary}`;
+//         },
+//     };
+// };
+
+// const salaryManager = salaryManagerFactor("Mango", 5000);
+// console.log(salaryManager.current())
+
+// console.dir(salaryManager.current)
+
+// salaryManager.raise(2000)
+
+// console.log(salaryManager.current())
+
+// salaryManager.lower(3000)
+
+// console.log(salaryManager.current())
+
+// console.log(salaryManager.raise(5000))
+
+// console.log(salaryManager.current())
+
+// -----------------стрілочна функція---------------
+// 1) якщо не має імені параметра - ставиом ()
+// 2) якщо один параметр - можна не ставити дужок
+// 3) якщо декілька параметрів обов'язково ставимо дужки (a, b, c)
+// 4) у стрілочнної функції нема arguments тому вони збираються через resp (...args)
+
+// const free = function (a,b,c) => a + b + c; 
+
+const add = function ( a, b, c ) {
+    console.log(arguments)
+    return a + b + c;
+};
+
+const addArrow = (...args) => {
+    console.log(args)
+}
+
+console.log(add(5, 10, 15 ));
+
+console.log(addArrow(3, 4, 2));
+
+
+
+
+
+
