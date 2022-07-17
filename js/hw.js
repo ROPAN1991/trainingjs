0a99d15671da23da40cc032756a33c2de1794587
ROPAN1991 <91415292+ROPAN1991@users.noreply.github.com>
2022-07-17 22:04:56 +0300

    practic

--- js/hw.js
+++ js/hw.js
@@ -2562,3 +2562,27 @@
   
 //     return message;
 //   }
+
+// function getSubscriptionPrice(type) {
+//     let price;
+//     // Change code below this line
+  
+//    switch (type) { // Change this line
+//       case 'starter': // Change this line
+//         price = 0; // Change this line
+//         break;
+  
+//       case 'professional': // Change this line
+//         price = 20; // Change this line
+//         break;
+  
+//       case 'organization': // Change this line
+//         price = 50; // Change this line
+//         break;
+//     }
+  
+//     // Change code above this line
+//     return price;
+//   }
+
+ 
