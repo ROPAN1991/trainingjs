3ab818bebd5c688326668013a19865f2221c20ce
ROPAN1991 <91415292+ROPAN1991@users.noreply.github.com>
2022-07-23 17:06:09 +0300

    newcommit

--- js/hw.js
+++ js/hw.js
@@ -3837,26 +3837,99 @@
 // null == "\n0\n" // false
 // null === +"\n0\n" // false
 
-1 == 1 // true
-'1' == 1 // true
-1 == '1' // true
-3 == 5 // false
-0 == false // true
-'foo' == 'bar' // false 
-1 != 2 // true
-1 != '1' // false
-'1' != 1 // false
-1 != true // false
-0 != false // false
-'foo' != 'bar' // true
-3 === 3 // true
-3 === '3' // false
-'foo' === 'foo' // true
-3 !== '3' // true
-4 !== 3 // ture
-4 > 3 // true
-1 > 5 // false
-4 >= 3 // true
-3 >= 3 // true
-3 <= 4 // true
-3 <= 3 // true
+// 1 == 1 // true
+// '1' == 1 // true
+// 1 == '1' // true
+// 3 == 5 // false
+// 0 == false // true
+// 'foo' == 'bar' // false 
+// 1 != 2 // true
+// 1 != '1' // false
+// '1' != 1 // false
+// 1 != true // false
+// 0 != false // false
+// 'foo' != 'bar' // true
+// 3 === 3 // true
+// 3 === '3' // false
+// 'foo' === 'foo' // true
+// 3 !== '3' // true
+// 4 !== 3 // ture
+// 4 > 3 // true
+// 1 > 5 // false
+// 4 >= 3 // true
+// 3 >= 3 // true
+// 3 <= 4 // true
+// 3 <= 3 // true
+
+// let year = prompt('В каком году была опубликована спецификация ECMAScript-2015?', '');
+
+// if (year < 2015) {
+//   alert( 'Это слишком рано...' );
+// } else if (year > 2015) {
+//   alert( 'Это поздновато' );
+// } else {
+//   alert( 'Верно!' );
+// }
+
+// let findAge;
+
+// let age = prompt("Який ваш вій?", "")
+
+// if (age > 18) {
+//     findAge = true;
+// } else {
+//     findAge = false;
+// }
+
+// alert(findAge)
+
+// let age = prompt('Возраст?', 18);
+
+// let message = (age < 3) ? 'Здравствуй, малыш!' :
+//   (age < 18) ? 'Привет!' :
+//   (age < 100) ? 'Здравствуйте!' :
+//   'Какой необычный возраст!';
+
+// alert( message );
+
+// let ask;
+
+// let question = prompt("Яка офіційна мова JavaScript?", "")
+
+// if (question === "ECMAscript") {
+//   ask = "Вірно"
+// } else {
+//     ask = "Не знаєте ECMAscript?"
+// }
+
+// alert(ask)
+
+// Используя конструкцию if..else, напишите код, который получает число через prompt, а затем выводит в alert:
+
+// 1, если значение больше нуля,
+// -1, если значение меньше нуля,
+// 0, если значение равно нулю.
+// Предполагается, что пользователь вводит только числа.
+
+// let number = prompt("Отримуємо число")
+
+// if(number > 0) {
+//     alert(1)
+// } else if (number < 0) {
+//     alert(-1)
+// } else {
+//     alert(0)
+// }
+
+
+// let result = (a + b < 4) ? 'Мало' : 'Много';
+
+
+
+// let message = (login == 'Сотрудник') ? 'Привет' : 
+// (login == 'Директор') ? 'Здравствуйте' : 
+// (login == '') ? 'Нет логина' :
+// "";
+
+
+
