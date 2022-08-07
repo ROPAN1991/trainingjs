a2dfceba31d056a4e10026adcbbd1ba7eb038de1
ROPAN1991 <91415292+ROPAN1991@users.noreply.github.com>
2022-08-07 16:37:22 +0300

    newcommit

--- js/hw.js
+++ js/hw.js
@@ -4470,3 +4470,60 @@
 // }
 
 // alert( extractCurrencyValue('$120') === 120 ); // true
+
+// ["Bilbo", "Gandalf", "Nazgul"].forEach((item, index, array) => {
+//   alert(`${item} имеет позицию ${index} в ${array}`);
+// });
+
+// function camelize (str) {
+//   return str.split("-").map((word, index) => index == 0 ? word : word[0].toUpperCase + word.slice(1)).join('');
+// }
+
+// function filterRangeInPlace(arr, a, b) {
+
+//   for (let i = 0; i < arr.length; i++) {
+//     console.log(arr)
+//     let val = arr[i];
+//     console.log(val)
+
+//     // удалить, если за пределами интервала
+//     if (val < a || val > b) {
+//       arr.splice(i, 1);
+//       i--;
+//     }
+//   }
+
+// }
+
+// let arr = [5, 3, 8, 1];
+
+// filterRangeInPlace(arr, 1, 4); // удалены числа вне диапазона 1..4
+
+// console.log( arr ); // [3, 1]
+
+// function Calculator() {
+
+//   this.methods = {
+//     "-": (a, b) => a - b,
+//     "+": (a, b) => a + b
+//   };
+
+//   this.calculate = function(str) {
+
+//     let split = str.split(' '),
+//       a = +split[0],
+//       op = split[1],
+//       b = +split[2]
+      
+
+//     if (!this.methods[op] || isNaN(a) || isNaN(b)) {
+//       return NaN;
+//     }
+
+//     return this.methods[op](a, b);
+//   }
+
+//   this.addMethod = function(name, func) {
+//     this.methods[name] = func;
+//   };
+// }
