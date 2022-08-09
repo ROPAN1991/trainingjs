c615dce4b43d297bf3a1738cb7058224ce032afb
ROPAN1991 <91415292+ROPAN1991@users.noreply.github.com>
2022-08-09 11:16:43 +0300

    new

--- js/hw.js
+++ js/hw.js
@@ -17,3 +17,16 @@
 // }
 
 // console.table(friends)
+
+// знайти суму всіх продуктів
+
+// const cart = [50, 44, 13, 28, 19, 71, 120]
+
+// let total = 0;
+
+// for (let i = 0; i < cart.length; i = i + 1) {
+//     console.log(cart[i])
+
+//     total = total + cart[i]
+// }
+
