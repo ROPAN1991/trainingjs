d6acb607208b81f7534596733f1f0ae26c5667c4
ROPAN1991 <91415292+ROPAN1991@users.noreply.github.com>
2022-10-06 14:08:16 +0300

    commit

--- js/hw.js
+++ js/hw.js
@@ -1,14 +1,18 @@
-const apartment = {
-  descr: "Spacious apartment in the city center",
-  rating: 4,
-  price: 2153,
-};
-const keys = [];
-for(let key in apartment) {
-  keys.push(key)
+// Напиши функцію countProps(object), яка рахує і повертає кількість власних властивостей об'єкта в параметрі object. Використовуй змінну propCount для зберігання кількості властивостей об'єкта.
+
+function countProps(object) {
+  let propCount = [];
+  // Change code below this line
+  // const keys = Object.keys(object)
+  // propCount = keys.length
+  for(let keys in object){
+    if(object.hasOwnProperty(keys))
+    propCount.push(keys)
+  }
+  // Change code above this line
+  return propCount.length;
 }
 
-const values = [];
-for(let value in apartment){
-  values.push(apartment[value])
-}
+console.log(countProps({}));
+console.log(countProps({ name: "Mango", age: 2 }));
+console.log(countProps({ mail: "poly@mail.com", isOnline: true, score: 500 }))
