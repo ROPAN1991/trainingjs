4e99a4189249fadeeaa52e258e76598e042c9c6a
ROPAN1991 <91415292+ROPAN1991@users.noreply.github.com>
2022-10-10 16:31:10 +0300

    commit

--- js/hw.js
+++ js/hw.js
@@ -1,5 +1,3 @@
-// Напиши функцію getProductPrice(productName), яка приймає один параметр productName - назва продукту. Функція шукає об'єкт продукту з таким ім'ям (властивість name) в масиві products і повертає його ціну (властивість price). Якщо продукт з такою назвою не знайдений, функція повинна повертати null.
-
 const products = [
   { name: "Radar", price: 1300, quantity: 4 },
   { name: "Scanner", price: 2700, quantity: 3 },
@@ -7,21 +5,17 @@ const products = [
   { name: "Grip", price: 1200, quantity: 9 },
 ];
 
-function getProductPrice(productName) {
-
-let price = 0
-
-for(const product of products){
-  console.log(product)
-  if(product.name.includes(productName)){
-    price = product.price
+function getAllPropValues(propName) {
+  let mass = [];
+  for (const product of products) {
+    let keys = Object.keys(product);
+    for (const key of keys) {
+      if (propName === key) {
+        mass.push(product[key]);
+      }
+    }
   }
-  
+  return mass;
 }
 
-return price
-}
-
-console.log(getProductPrice("Radar"))
-console.log(getProductPrice("Engine"))
 
