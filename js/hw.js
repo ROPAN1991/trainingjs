d87e9b8cdb80a0fc66a758efb3e888d16d208597
ROPAN1991 <91415292+ROPAN1991@users.noreply.github.com>
2022-10-12 18:49:37 +0300

    commit43

--- js/hw.js
+++ js/hw.js
@@ -1,37 +1,49 @@
-// Напиши функцію makeTask(data) яка приймає один параметр data - об'єкт з наступними властивостями.
+// // Напиши функцію makeTask(data) яка приймає один параметр data - об'єкт з наступними властивостями.
 
-// text - текст завдання.
-// category - категорія завдання.
-// priority - пріоритет завдання.
-// Функція повинна створити і повернути новий об'єкт завдання, не змінюючи напряму параметр data. У новому об'єкті повинна бути властивість completed, значення якої зберігається в однойменній локальній змінній.
+// // text - текст завдання.
+// // category - категорія завдання.
+// // priority - пріоритет завдання.
+// // Функція повинна створити і повернути новий об'єкт завдання, не змінюючи напряму параметр data. У новому об'єкті повинна бути властивість completed, значення якої зберігається в однойменній локальній змінній.
 
-// В параметрі data гарантовано буде тільки властивість text, а інші дві, category і priority, можуть бути відсутніми. Тоді, в новому об'єкті завдання, у властивостях category і priority повинні бути значення за замовчуванням, що зберігаються в однойменних локальних змінних.
+// // В параметрі data гарантовано буде тільки властивість text, а інші дві, category і priority, можуть бути відсутніми. Тоді, в новому об'єкті завдання, у властивостях category і priority повинні бути значення за замовчуванням, що зберігаються в однойменних локальних змінних.
 
-function makeTask(data) {
-  const completed = false;
-  const category = "General";
-  const priority = "Normal";
+// function makeTask(data) {
+//   const completed = false;
+//   const category = "General";
+//   const priority = "Normal";
 
-  const newObject = {};
+//   const newObject = {};
 
-  for (const key in data) {
-    newObject[key] = data[key];
-    // console.log(newObject[key])
-    console.log(data[key])
-  }
-  newObject.completed = completed 
-  newObject.category = newObject.category || category
+//   for (const key in data) {
+//     newObject[key] = data[key];
+//     // console.log(newObject[key])
+//     console.log(data[key])
+//   }
+//   newObject.completed = completed
+//   newObject.category = newObject.category || category
 
-  
-  newObject.priority = newObject.priority || priority
+//   newObject.priority = newObject.priority || priority
 
-  return newObject;
-}
+//   return newObject;
+// }
 
+// // console.log(makeTask({}));
+// // console.log(makeTask({ category: "Finance", text: "Take interest" }));
+// // console.log(makeTask({ category: "Homemade", priority: "Low", text: "Take out the trash" }))
+// // console.log(makeTask({ category: "Finance", text: "Take interest" }))
+// // console.log(makeTask({ priority: "Low", text: "Choose shampoo" }))
+// // console.log(makeTask({ text: "Buy bread" }))
 
-// console.log(makeTask({}));
-// console.log(makeTask({ category: "Finance", text: "Take interest" }));
-// console.log(makeTask({ category: "Homemade", priority: "Low", text: "Take out the trash" }))
-// console.log(makeTask({ category: "Finance", text: "Take interest" }))
-// console.log(makeTask({ priority: "Low", text: "Choose shampoo" }))
-// console.log(makeTask({ text: "Buy bread" }))
+// function task(data) {
+//   const array = []
+
+//   for(const object of data) {
+//     if (object.id !== undefined) {
+//       array.push({...object, age: 25})
+//     }
+//   }
+//   return array
+
+// }
+
+// console.log(task([{ id: 11 }, { id: 2 }, { name: "Anton" }]));
