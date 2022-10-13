5e9cce1a671467eb5016d18808d6cfb0d6ea28ec
ROPAN1991 <91415292+ROPAN1991@users.noreply.github.com>
2022-10-13 19:11:47 +0300

    commit2

--- js/hw.js
+++ js/hw.js
@@ -47,3 +47,23 @@
 // }
 
 // console.log(task([{ id: 11 }, { id: 2 }, { name: "Anton" }]));
+
+// Функція addOverNum() рахує суму всіх аргументів. Зміни параметри і тіло функції addOverNum() таким чином, щоб вона рахувала суму тільки тих аргументів, які більші за задане число. Це число повинно бути першим параметром функції.
+
+function addOverNum(...args) {
+    let total = 0; 
+  
+    for (const arg of args) {
+      if(args[0] < arg) {
+        total += arg
+      }
+    }
+  
+    return total;
+   
+  }
+
+console.log(addOverNum(50, 15, 27));
+console.log(addOverNum(10, 12, 4, 11, 48, 10, 8));
+console.log(addOverNum(15, 32, 6, 13, 19, 8));
+console.log(addOverNum(20, 74, 11, 62, 46, 12, 36));
