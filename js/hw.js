3b7f85d49da7dc20b911d8d22a8131d5f0ddfd57
ROPAN1991 <91415292+ROPAN1991@users.noreply.github.com>
2022-10-15 11:37:17 +0300

    newcommit2

--- js/hw.js
+++ js/hw.js
@@ -119,15 +119,33 @@
 
 // Доповни метод removePotion(potionName) таким чином, щоб він видаляв зілля potionName з масиву зілля у властивості potions.
 
-const atTheOldToad = {
-  potions: ["Speed potion", "Dragon breath", "Stone skin"],
-  removePotion(potionName) {
-    // Change code below this line
-    const indexOfPotion = this.potions.indexOf(potionName)
-    this.potions.splice(indexOfPotion, 1)
+// const atTheOldToad = {
+//   potions: ["Speed potion", "Dragon breath", "Stone skin"],
+//   removePotion(potionName) {
+//     // Change code below this line
+//     const indexOfPotion = this.potions.indexOf(potionName)
+//     this.potions.splice(indexOfPotion, 1)
 
-    // Change code above this line
-  },
-};
+//     // Change code above this line
+//   },
+// };
+
+// const atTheOldToad = {
+//   potions: ["Speed potion", "Dragon breath", "Stone skin"],
+//   updatePotionName(oldName, newName) {
+//     const indexOfName = this.potions.indexOf(oldName)
+//     this.potions.splice(indexOfName, 1, newName)
+//   },
+// };
+
+// console.log(atTheOldToad.potions)
+// console.log(atTheOldToad.updatePotionName("Dragon breath", "Polymorth"))
+
+// Виконай рефакторинг методів об'єкта atTheOldToad таким чином, щоб вони працювали не з масивом рядків, а з масивом об'єктів.
+
+// getPotions() - метод для отримання всього зілля. Повертає значення властивості potions.
+// addPotion(newPotion) - додає зілля newPotion (вже об'єкт) в масив у властивості potions, але тільки, якщо такого зілля ще немає в інвентарі. В іншому випадку повертається рядок.
+// removePotion(potionName) - видаляє об'єкт зілля з ім'ям potionName з масиву у властивості potions.
+// updatePotionName(oldName, newName) - оновлює властивість name об'єкта-зілля з назвою oldName на newName в масиві potions.
 
 
