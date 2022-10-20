94b531b9e8c7fd88769207e05f74cbdc7a604c9b
ROPAN1991 <91415292+ROPAN1991@users.noreply.github.com>
2022-10-20 19:16:36 +0300

    callback1

--- js/hw.js
+++ js/hw.js
@@ -141,14 +141,34 @@
 // console.log(atTheOldToad.potions)
 // console.log(atTheOldToad.updatePotionName("Dragon breath", "Polymorth"))
 
-// Change code below this line
-function add(...args) {
-  let sum = 0
-  for(const argument of args) {
-    sum += argument
+
+
+function processCall(recipient, onAvailable, onNotAvailable) {
+  // Імітуємо доступність абонента випадковим числом
+  const isRecipientAvailable = Math.random() > 0.5;
+
+  if (!isRecipientAvailable) {
+    onNotAvailable(recipient);
+    return;
   }
-  return sum
-  // Change code above this line
+
+  onAvailable(recipient);
 }
 
-console.log(add(15, 27))
+function takeCall(name) {
+  console.log(`З'єднуємо з ${name}, очікуйте...`);
+  // Логіка прийняття дзвінка
+}
+
+function activateAnsweringMachine(name) {
+  console.log(`Абонент ${name} недоступний, залиште повідомлення.`);
+  // Логіка активації автовідповідача
+}
+
+function leaveHoloMessage(name) {
+  console.log(`Абонент ${name} недоступний, записуємо голограму.`);
+  // Логіка запису голограми
+}
+
+processCall("Манго", takeCall, activateAnsweringMachine);
+processCall("Полі", takeCall, leaveHoloMessage);
