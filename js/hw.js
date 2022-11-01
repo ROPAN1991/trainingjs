2a3ed99737dc3cb7be513a586925561018ddfd43
ROPAN1991 <91415292+ROPAN1991@users.noreply.github.com>
2022-11-01 16:41:07 +0200

    newcommit

--- js/hw.js
+++ js/hw.js
@@ -176,14 +176,42 @@
 // const biggestValue = values.filter(value => value > 1000)
 // console.log(biggestValue)
 
-const numbers = [1,2,2,3,3,4,5]
-const oldSum = 20
+// const numbers = [1,2,2,3,3,4,5]
+// const oldSum = 20
 
-const filteredNumbers = numbers.filter((number, index, array) => array.indexOf(number) === index)
-console.log(filteredNumbers)
+// const filteredNumbers = numbers.filter((number, index, array) => array.indexOf(number) === index)
+// console.log(filteredNumbers)
 
-const reducedFilteredNumbers = filteredNumbers.reduce((acc, value) => oldSum + acc + value)
+// const reducedFilteredNumbers = filteredNumbers.reduce((acc, value) => oldSum + acc + value)
 
-console.log(reducedFilteredNumbers)
+// console.log(reducedFilteredNumbers)
 
+// Необхідно написати логіку обробки замовлення піци. Виконай рефакторинг методу order таким чином, щоб він приймав другим і третім параметром два колбеки onSuccess і onError.
 
+// Якщо у властивості pizzas відсутня піца з назвою з параметра pizzaName, метод order повинен повертати результат виклику колбека onError, передаючи йому аргументом рядок "There is no pizza with a name <имя пиццы> in the assortment."
+// Якщо у властивості pizzas присутня піца з назвою з параметра pizzaName, метод order повинен повертати результат виклику колбека onSuccess, передаючи йому аргументом назву замовленої піци.
+// Після оголошення об'єкта pizzaPalace ми додали колбеки і виклики методів. Будь ласка, нічого там не міняй.
+
+const pizzaPalace = {
+  pizzas: ["Ultracheese", "Smoked", "Four meats"],
+  order(pizzaName, onSuccess, onError) {
+    if (this.pizzas.includes(pizzaName)) {
+      return onSuccess(pizzaName);
+    } else {
+      return onError(pizzaName);
+    }
+  },
+};
+
+function makePizza(pizzaName) {
+  return `Your order is accepted. Cooking pizza ${pizzaName}.`;
+}
+
+function onOrderError(pizzaName) {
+  return `Error! There is no pizza with a name ${pizzaName} in the assortment.`;
+}
+
+console.log(pizzaPalace.order("Smoked", makePizza, onOrderError));
+console.log(pizzaPalace.order("Four meats", makePizza, onOrderError));
+console.log(pizzaPalace.order("Big Mike", makePizza, onOrderError));
+console.log(pizzaPalace.order("Vienna", makePizza, onOrderError));
