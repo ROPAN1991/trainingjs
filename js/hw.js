283156455b3cdd45983d02596fc37bb12eb3ea35
ROPAN1991 <91415292+ROPAN1991@users.noreply.github.com>
2022-11-14 14:54:10 +0200

    commit234

--- js/hw.js
+++ js/hw.js
@@ -230,7 +230,157 @@
 
 // console.log(changeEven([1, 2, 3, 4, 5], 10))
 
-const users = [
+// const users = [
+//   {
+//     name: "Moore Hensley",
+//     email: "moorehensley@indexia.com",
+//     eyeColor: "blue",
+//     friends: ["Sharron Pace"],
+//     isActive: false,
+//     balance: 2811,
+//     gender: "male",
+//     age: 37,
+//   },
+//   {
+//     name: "Sharlene Bush",
+//     email: "sharlenebush@tubesys.com",
+//     eyeColor: "blue",
+//     friends: ["Briana Decker", "Sharron Pace"],
+//     isActive: true,
+//     balance: 3821,
+//     gender: "female",
+//     age: 34,
+//   },
+//   {
+//     name: "Ross Vazquez",
+//     email: "rossvazquez@xinware.com",
+//     eyeColor: "green",
+//     friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+//     isActive: false,
+//     balance: 3793,
+//     gender: "male",
+//     age: 24,
+//   },
+//   {
+//     name: "Elma Head",
+//     email: "elmahead@omatom.com",
+//     eyeColor: "green",
+//     friends: ["Goldie Gentry", "Aisha Tran"],
+//     isActive: true,
+//     balance: 2278,
+//     gender: "female",
+//     age: 21,
+//   },
+//   {
+//     name: "Carey Barr",
+//     email: "careybarr@nurali.com",
+//     eyeColor: "blue",
+//     friends: ["Jordan Sampson", "Eddie Strong"],
+//     isActive: true,
+//     balance: 3951,
+//     gender: "male",
+//     age: 27,
+//   },
+//   {
+//     name: "Blackburn Dotson",
+//     email: "blackburndotson@furnigeer.com",
+//     eyeColor: "brown",
+//     friends: ["Jacklyn Lucas", "Linda Chapman"],
+//     isActive: false,
+//     balance: 1498,
+//     gender: "male",
+//     age: 38,
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     email: "shereeanthony@kog.com",
+//     eyeColor: "brown",
+//     friends: ["Goldie Gentry", "Briana Decker"],
+//     isActive: true,
+//     balance: 2764,
+//     gender: "female",
+//     age: 39,
+//   },
+// ]
+  // const getUsers = (users) => {
+  //     return users.flatMap(user => user.friends).filter((friend, index, array) => array.indexOf(friend) === index)
+  // }
+
+  // console.log(getUsers(users))
+
+  // const isEveryUserActive = (users) => {
+  //     return users.every(user => user.isActive)
+  //  };
+
+  // const players = {
+  //     mango: 1270,
+  //     poly: 468,
+  //     ajax: 710,
+  //     kiwi: 244
+  //   };
+  //   const playtimes = Object.values(players); // [1270, 468, 710, 244]
+  //   // Change code below this line
+
+  //   const totalPlayTime = playtimes;
+
+  //   // Change code above this line
+  //   const averagePlayTime = totalPlayTime / playtimes.length;
+
+  // const players = [
+  //     { name: "Mango", playtime: 1270, gamesPlayed: 4 },
+  //     { name: "Poly", playtime: 469, gamesPlayed: 2 },
+  //     { name: "Ajax", playtime: 690, gamesPlayed: 3 },
+  //     { name: "Kiwi", playtime: 241, gamesPlayed: 1 },
+  //   ];
+  //   // Change code below this line
+
+  //   const totalAveragePlaytimePerGame = players.reduce((acc, player) => acc += player.playtime / player.gamesPlayed, 0);
+
+  //   const players = [
+  //     { name: "Mango", playtime: 1270, gamesPlayed: 4 },
+  //     { name: "Poly", playtime: 469, gamesPlayed: 2 },
+  //     { name: "Ajax", playtime: 690, gamesPlayed: 3 },
+  //     { name: "Kiwi", playtime: 241, gamesPlayed: 1 },
+  //   ];
+
+  //   const totalAveragePlaytimePerGame = players.reduce((acc, player) => {return acc + player.playtime / player.gamesPlayed}, 0)
+
+  //   console.log(totalAveragePlaytimePerGame)
+
+  // Доповни код таким чином, щоб у змінній names вийшов масив імен авторів в алфавітному порядку, рейтинг книг яких більший за значення змінної MIN_BOOK_RATING.
+
+  // const books = [
+  //   {
+  //     title: "The Last Kingdom",
+  //     author: "Bernard Cornwell",
+  //     rating: 8.38,
+  //   },
+  //   {
+  //     title: "Beside Still Waters",
+  //     author: "Robert Sheckley",
+  //     rating: 8.51,
+  //   },
+  //   {
+  //     title: "The Dream of a Ridiculous Man",
+  //     author: "Fyodor Dostoevsky",
+  //     rating: 7.75,
+  //   },
+  //   { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
+  //   {
+  //     title: "The Dreams in the Witch House",
+  //     author: "Howard Lovecraft",
+  //     rating: 8.67,
+  //   },
+  // ];
+  // const MIN_BOOK_RATING = 8;
+  // // Change code below this line
+
+  // const names = books.filter(book => book.rating > MIN_BOOK_RATING).map(book => book.author).sort((a,b) => a.localeCompare(b))
+  // console.log(names)
+
+  // Доповни функцію getNamesSortedByFriendCount(users) таким чином, щоб вона повертала масив імен користувачів, відсортований за зростанням кількості їхніх друзів (властивість friends).
+
+  const users = [
     {
       name: "Moore Hensley",
       email: "moorehensley@indexia.com",
@@ -238,8 +388,7 @@ const users = [
       friends: ["Sharron Pace"],
       isActive: false,
       balance: 2811,
-      gender: "male",
-      age: 37
+      gender: "male"
     },
     {
       name: "Sharlene Bush",
@@ -248,8 +397,7 @@ const users = [
       friends: ["Briana Decker", "Sharron Pace"],
       isActive: true,
       balance: 3821,
-      gender: "female",
-      age: 34
+      gender: "female"
     },
     {
       name: "Ross Vazquez",
@@ -258,8 +406,7 @@ const users = [
       friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
       isActive: false,
       balance: 3793,
-      gender: "male",
-      age: 24
+      gender: "male"
     },
     {
       name: "Elma Head",
@@ -268,28 +415,25 @@ const users = [
       friends: ["Goldie Gentry", "Aisha Tran"],
       isActive: true,
       balance: 2278,
-      gender: "female",
-      age: 21
+      gender: "female"
     },
     {
       name: "Carey Barr",
       email: "careybarr@nurali.com",
       eyeColor: "blue",
-      friends: ["Jordan Sampson", "Eddie Strong"],
+      friends: ["Jordan Sampson", "Eddie Strong", "Adrian Cross"],
       isActive: true,
       balance: 3951,
-      gender: "male",
-      age: 27
+      gender: "male"
     },
     {
       name: "Blackburn Dotson",
       email: "blackburndotson@furnigeer.com",
       eyeColor: "brown",
-      friends: ["Jacklyn Lucas", "Linda Chapman"],
+      friends: ["Jacklyn Lucas", "Linda Chapman", "Adrian Cross", "Solomon Fokes"],
       isActive: false,
       balance: 1498,
-      gender: "male",
-      age: 38
+      gender: "male"
     },
     {
       name: "Sheree Anthony",
@@ -298,56 +442,20 @@ const users = [
       friends: ["Goldie Gentry", "Briana Decker"],
       isActive: true,
       balance: 2764,
-      gender: "female",
-      age: 39
+      gender: "female"
     }
   ]
 
-// const getUsers = (users) => {
-//     return users.flatMap(user => user.friends).filter((friend, index, array) => array.indexOf(friend) === index)
-// }
-
-// console.log(getUsers(users))
-
-
-// const isEveryUserActive = (users) => {
-//     return users.every(user => user.isActive)
+// const getNamesSortedByFriendCount = users => {
+//   return [...users].sort((a,b) => a.friends.length - b.friends.length).map(user => user.name)  
 //  };
 
-// const players = {
-//     mango: 1270,
-//     poly: 468,
-//     ajax: 710,
-//     kiwi: 244
-//   };
-//   const playtimes = Object.values(players); // [1270, 468, 710, 244]
-//   // Change code below this line
-  
-//   const totalPlayTime = playtimes;
-  
-//   // Change code above this line
-//   const averagePlayTime = totalPlayTime / playtimes.length;
+// console.log(getNamesSortedByFriendCount(users));
 
-// const players = [
-//     { name: "Mango", playtime: 1270, gamesPlayed: 4 },
-//     { name: "Poly", playtime: 469, gamesPlayed: 2 },
-//     { name: "Ajax", playtime: 690, gamesPlayed: 3 },
-//     { name: "Kiwi", playtime: 241, gamesPlayed: 1 },
-//   ];
-//   // Change code below this line
-  
-//   const totalAveragePlaytimePerGame = players.reduce((acc, player) => acc += player.playtime / player.gamesPlayed, 0);
-  
+// Доповни функцію getTotalBalanceByGender(users, gender) таким чином, щоб вона повертала загальний баланс користувачів (властивість balance), стать яких (властивість gender) збігається зі значенням параметра gender.
 
+const getTotalBalanceByGender = (users, gender) => {
+  return users.reduce((total, user) => total += user.balance, 0)
+};
 
-//   const players = [
-//     { name: "Mango", playtime: 1270, gamesPlayed: 4 },
-//     { name: "Poly", playtime: 469, gamesPlayed: 2 },
-//     { name: "Ajax", playtime: 690, gamesPlayed: 3 },
-//     { name: "Kiwi", playtime: 241, gamesPlayed: 1 },
-//   ];
-
-//   const totalAveragePlaytimePerGame = players.reduce((acc, player) => {return acc + player.playtime / player.gamesPlayed}, 0)
-
-//   console.log(totalAveragePlaytimePerGame)
-
+console.log(getTotalBalanceByGender(users))
