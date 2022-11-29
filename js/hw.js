45cf2e7bd6a555c17aef793aa6f53b2a58b8431a
ROPAN1991 <91415292+ROPAN1991@users.noreply.github.com>
2022-11-29 09:47:26 +0200

    commit1

--- js/hw.js
+++ js/hw.js
@@ -618,28 +618,105 @@
 
 // Виконай рефакторинг функції таким чином, щоб вона стала чистою - не змінювала масив чисел numbers, а створювала, наповнювала і повертала новий масив з оновленими значеннями.
 
-function changeEven(numbers, value) {
-  const newArray = []
-  // Change code below this line
+// function changeEven(numbers, value) {
+//   const newArray = []
+//   // Change code below this line
 
-  numbers.forEach(number => {
-    if(number % 2 === 0) {
-      number += value
-    }
-    newArray.push(number)
-  })
-
-
-  // for (let i = 0; i < numbers.length; i += 1) {
-  //   if (numbers[i] % 2 === 0) {
-  //     numbers[i] = numbers[i] + value;
-  //   }
-  // }
+//   numbers.forEach(number => {
+//     if(number % 2 === 0) {
+//       number += value
+//     }
+//     newArray.push(number)
+//   })
 
 
-  // Change code above this line
-  return newArray
+//   // for (let i = 0; i < numbers.length; i += 1) {
+//   //   if (numbers[i] % 2 === 0) {
+//   //     numbers[i] = numbers[i] + value;
+//   //   }
+//   // }
+
+
+//   // Change code above this line
+//   return newArray
+// }
+
+// console.log(changeEven([1, 2, 3, 4, 5], 10))
+
+// Доповни функцію getNamesSortedByFriendCount(users) таким чином, щоб вона повертала масив імен користувачів, відсортований за зростанням кількості їхніх друзів (властивість friends).
+
+const users = [
+  {
+    name: "Moore Hensley",
+    email: "moorehensley@indexia.com",
+    eyeColor: "blue",
+    friends: ["Sharron Pace"],
+    isActive: false,
+    balance: 2811,
+    gender: "male"
+  },
+  {
+    name: "Sharlene Bush",
+    email: "sharlenebush@tubesys.com",
+    eyeColor: "blue",
+    friends: ["Briana Decker", "Sharron Pace"],
+    isActive: true,
+    balance: 3821,
+    gender: "female"
+  },
+  {
+    name: "Ross Vazquez",
+    email: "rossvazquez@xinware.com",
+    eyeColor: "green",
+    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+    isActive: false,
+    balance: 3793,
+    gender: "male"
+  },
+  {
+    name: "Elma Head",
+    email: "elmahead@omatom.com",
+    eyeColor: "green",
+    friends: ["Goldie Gentry", "Aisha Tran"],
+    isActive: true,
+    balance: 2278,
+    gender: "female"
+  },
+  {
+    name: "Carey Barr",
+    email: "careybarr@nurali.com",
+    eyeColor: "blue",
+    friends: ["Jordan Sampson", "Eddie Strong", "Adrian Cross"],
+    isActive: true,
+    balance: 3951,
+    gender: "male"
+  },
+  {
+    name: "Blackburn Dotson",
+    email: "blackburndotson@furnigeer.com",
+    eyeColor: "brown",
+    friends: ["Jacklyn Lucas", "Linda Chapman", "Adrian Cross", "Solomon Fokes"],
+    isActive: false,
+    balance: 1498,
+    gender: "male"
+  },
+  {
+    name: "Sheree Anthony",
+    email: "shereeanthony@kog.com",
+    eyeColor: "brown",
+    friends: ["Goldie Gentry", "Briana Decker"],
+    isActive: true,
+    balance: 2764,
+    gender: "female"
+  }
+]
+
+const getNamesSortedByFriendCount = users => {
+  
+   return [...users].sort((a, b) => a.friends.length - b.friends.length).map(user => user.name)
+   
 }
 
-console.log(changeEven([1, 2, 3, 4, 5], 10))
+console.log(getNamesSortedByFriendCount(users))
+
 
