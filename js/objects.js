00aad3134f522166994c0251ab978f442d3d01d2
ROPAN1991 <91415292+ROPAN1991@users.noreply.github.com>
2023-03-27 15:33:14 +0300

    commit5

--- js/objects.js
+++ js/objects.js
@@ -390,22 +390,3 @@
 //     }
 //   }
 
-class User {
-    static #takenEmails = [];
-  
-    static isEmailTaken(email) {
-      return User.#takenEmails.includes(email);
-    }
-  
-    #email;
-  
-    constructor({ email }) {
-      this.#email = email;
-      User.#takenEmails.push(email);
-    }
-  }
-  
-  const mango = new User({ email: "mango@mail.com" });
-  
-  console.log(User.isEmailTaken("poly@mail.com"));
-  console.log(User.isEmailTaken("mango@mail.com"));
