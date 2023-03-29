5450e938754e0d76fb093d7aa5f8d1a3ae9322dd
ROPAN1991 <91415292+ROPAN1991@users.noreply.github.com>
2023-03-29 15:42:15 +0300

    commit1

--- js/objects.js
+++ js/objects.js
@@ -360,6 +360,41 @@
 // console.log(book.author)
 // console.log(bookAuthor)
 
-const mango = {
-    name: "Mango"
-}
+class Car {
+    // Change code below this line
+    
+    
+    #price;
+  
+    constructor({ price }) {
+      this.#price = price;
+    }
+  
+    get price() {
+      return this.#price;
+    }
+  
+    set price(newPrice) {
+      if(newPrice > Car.Price.MAX_PRICE) {
+      
+      this.#price = newPrice;
+      }
+    }
+    // Change code above this line
+  }
+  
+  const audi = new Car({ price: 35000 });
+  console.log(audi.price); // 35000
+  
+  audi.price = 49000;
+  console.log(audi.price); // 49000
+  
+  audi.price = 51000;
+  console.log(audi.price); // 49000
+  
+  
+
+  
+  
+
+  
