0b1b4c1384cd673aea462628d4314e8eaabc5b4f
ROPAN1991 <91415292+ROPAN1991@users.noreply.github.com>
2023-03-30 16:49:43 +0300

    commit2

--- js/objects.js
+++ js/objects.js
@@ -360,41 +360,16 @@
 // console.log(book.author)
 // console.log(bookAuthor)
 
-class Car {
-    // Change code below this line
-    
-    
-    #price;
-  
-    constructor({ price }) {
-      this.#price = price;
-    }
-  
-    get price() {
-      return this.#price;
-    }
-  
-    set price(newPrice) {
-      if(newPrice > Car.Price.MAX_PRICE) {
-      
-      this.#price = newPrice;
-      }
-    }
-    // Change code above this line
-  }
-  
-  const audi = new Car({ price: 35000 });
-  console.log(audi.price); // 35000
-  
-  audi.price = 49000;
-  console.log(audi.price); // 49000
-  
-  audi.price = 51000;
-  console.log(audi.price); // 49000
-  
-  
+console.log(document);
 
-  
-  
+const body = document.body;
+console.log(body);
 
-  
+const list = body.firstElementChild;
+console.log(list);
+
+const firstListItem = list.firstElementChild;
+console.log(firstListItem);
+
+const listItems = list.children;
+console.log(listItems);
