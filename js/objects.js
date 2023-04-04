87d942e703fd3463382e1f271c29310b9ca442a4
ROPAN1991 <91415292+ROPAN1991@users.noreply.github.com>
2023-04-04 19:33:06 +0300

    commit3

--- js/objects.js
+++ js/objects.js
@@ -360,15 +360,12 @@
 // console.log(book.author)
 // console.log(bookAuthor)
 
-const technologies = ["HTML", "CSS", "JavaScript", "React", "Node"];
-const list = document.querySelector(".list");
+const button = document.querySelector(".btn");
 
-const markup = technologies
-  .map((technology) => `<li class="list-item">${technology}</li>`)
-  .join("");
+const handleClick = (event) => {
+  console.log("event: ", event);
+  console.log("event type: ", event.type);
+  console.log("currentTarget: ", event.currentTarget);
+};
 
-// Check the console, you'll see a single string with HTML tags
-console.log(markup);
-
-// Adding all the markup in one operation
-list.innerHTML = markup;
+button.addEventListener("click", handleClick);
