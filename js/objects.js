e42c835bd8226f74571e7a21feb1519c981e1952
ROPAN1991 <91415292+ROPAN1991@users.noreply.github.com>
2023-04-18 17:26:31 +0300

    commit2

--- js/objects.js
+++ js/objects.js
@@ -356,6 +356,7 @@ const list = document.querySelector('.list')
 const newTechnologies = ['React', 'TypeScript', 'Node.js']
 
 const markup = newTechnologies.map(technology => `<li class="list-item new">${technology}</li>`).join("")
+console.log(markup)
 
 list.insertAdjacentHTML('beforeend', markup)
 
