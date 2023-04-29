f68454561be53b1544573efee6ce94a7d9490798
ROPAN1991 <91415292+ROPAN1991@users.noreply.github.com>
2023-04-29 12:02:46 +0300

    commit2

--- js/objects.js
+++ js/objects.js
@@ -586,10 +586,44 @@
 // }
 
 // function incrementKeypressCounter() {
-//   keypressCounter += 1;
-// }
+//     keypressCounter += 1;
+//   }
+
 
 // --------------------------------------------події клавіатури----------------------------------------------------------
 
+// const clearLogBtn = document.querySelector('[data-action="clear"]')
+// const logList = document.querySelector('.log-list')
+// let keypressCounter = 1
 
 
+// document.addEventListener('keydown', logMessage)
+// document.addEventListener("keyup", logMessage);
+// clearLogBtn.addEventListener('click', reset)
+
+
+// function logMessage ({type, key, code}) {
+//     const markup = `<div class="log-item">
+//     <span class="chip">${keypressCounter}</span>
+//         <ul>
+//             <li><b>Event</b>${type}</li>
+//             <li><b>Key</b>${key}</li>
+//             <li><b>Code</b>${code}</li>
+//         </ul>
+//     </div>`
+
+//     logList.insertAdjacentHTML('afterbegin', markup)
+
+//     if(key === "keyup") {
+//         incrementKeypressCounter()
+//     }
+// }
+
+// function incrementKeypressCounter() {
+//     keypressCounter += 1
+// }
+
+// function reset() {
+//     keypressCounter = 1
+//     logList.innerHTML = ""
+// }
